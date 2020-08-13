e039b4e1d2b1c13ea8318cb2e24aab9f3b5adda2
naarrek <h.narek@gmail.com>
2020-08-13 17:23:52 +0400

    linter errors fixed

--- theme/js/main.js
+++ theme/js/main.js
@@ -3,7 +3,7 @@ const selector = require("./_selector");
 document.addEventListener("DOMContentLoaded", selector.init, false);
 
 const hamburger = document.querySelector("#hamburger");
-hamburger.addEventListener("click", e =>
+hamburger.addEventListener("click", () =>
 {
   document.querySelector("#hamburger-menu").classList.toggle("visible");
 });
