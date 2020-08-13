00dc287408a7f13c469bca5f6e031959b562a2c2
naarrek <h.narek@gmail.com>
2020-08-13 17:16:55 +0400

    hamburger menu implemented

--- theme/js/main.js
+++ theme/js/main.js
@@ -1,3 +1,9 @@
 const selector = require("./_selector");
 
 document.addEventListener("DOMContentLoaded", selector.init, false);
+
+const hamburger = document.querySelector("#hamburger");
+hamburger.addEventListener("click", e =>
+{
+  document.querySelector("#hamburger-menu").classList.toggle("visible");
+});
