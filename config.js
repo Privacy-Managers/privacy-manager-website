21328f3aecfe2c0e06be415a42ac164de14b443a
naarrek <h.narek@gmail.com>
2020-08-20 02:26:50 +0400

    implemented part of review comments

--- config.js
+++ config.js
@@ -11,15 +11,8 @@ const templateData =
     localeMap: {
       "de": {name: "Deutsch", region: "de_DE", crowdin: "en-de"},
       "en": {name: "English (EN)", region: "en_US"},
-      "es-ES": {name: "Español", region: "es_ES", crowdin: "en-es"},
       "fr": {name: "Français", region: "fr_FR", crowdin: "en-fr"},
-      "hy-AM": {name: "Հայերեն", region: "hy_AM", crowdin: "en-hy"},
-      "it": {name: "Italiano", region: "it_IT", crowdin: "en-it"},
-      "lt": {name: "Lietuvių", region: "lt_LT", crowdin: "en-lt"},
-      "pl": {name: "Polski", region: "pl_PL", crowdin: "en-pl"},
-      "pt": {name: "Português", region: "pt_PT", crowdin: "en-pt"},
-      "ru": {name: "Русский (RU)", region: "ru_RU", crowdin: "en-ru"},
-      "zh-CN": {name: "中文", region: "zh_CN", crowdin: "en-zhcn"}
+      "ru": {name: "Русский (RU)", region: "ru_RU", crowdin: "en-ru"}
     },
     navigation: [
       {
